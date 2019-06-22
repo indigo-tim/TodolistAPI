af8f6b32e0972ece844192d2d6410d38f7b51324
indigo-tim <timothy.kim@indigoslate.com>
2019-06-22 14:10:12 -0700

    trying to find config

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const config = require('../config/config')
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.send('Forbidden');
+	res.send(config);
 });
 
 module.exports = router;
