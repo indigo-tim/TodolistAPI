410c32978027c19520ebd7ef8accdd427771135f
indigo-tim <timothy.kim@indigoslate.com>
2019-06-23 17:00:20 -0700

    push from yesterday updates

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //MAKE SURE THIS IS RESTRICTED IN PRODUCTION
 app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", process.env.HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const config = require('../config/config')
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.send(config);
+	res.send('Forbidden');
 });
 
 module.exports = router;
